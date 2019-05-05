7370e1a50e525ee10bea243041a35afb2c39db03
shweta <shwjais99@gmail.com>
2019-05-05 13:05:18 +0530

    Blog_part_2

--- blog_pro/blog_app/urls.py
+++ blog_pro/blog_app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from blog_app import views
+from . import views
 
 
 
--- blog_pro/blog_pro/urls.py
+++ blog_pro/blog_pro/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
-from blog_app import views
+# from blog_app import views
 from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog_app.urls')),
-    url(r'account/login/$',views.login,name='login'),
-    url(r'accounts/logout/$',views.logout,name='logout',kwargs={'next_page':'/'})
+    url(r'accounts/login/$',LoginView.as_view(),name='login'),
+    url(r'accounts/logout/$',LogoutView.as_view(),name='logout',kwargs={'next_page':'/'})
 ]
